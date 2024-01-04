5081941ec189280cdeed7c92e905ec48c9369448
NizerSentex <149303729+NizerSentex@users.noreply.github.com>
2024-01-04 19:12:27 +0530

    Create √sqrtcalc.cpp

---
+++ √sqrtcalc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void sqrts(int user_input ){
+   
+   cout<<"enter number:";
+   cin>>user_input;
+   int ṣresult=sqrt(user_input);
+   cout<<result;
+}
+
+int main(){
+    int ui;
+   sqrts( ui);
+   
+
+}
